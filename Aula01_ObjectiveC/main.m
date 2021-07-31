77e261af016d6b6cc9fbb866409cd6290050f054
Diego Ritzel <dipi.evil@gmail.com>
2021-07-31 11:07:05 -0300

    Initial Commit

---
+++ Aula01_ObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Aula01_ObjectiveC
+//
+//  Created by IOS SENAC on 31/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
