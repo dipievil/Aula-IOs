d7f4da4abf77d5f3b65a098e9fa1bf3f5a01a630
Diego Ritzel <dipi.evil@gmail.com>
2021-08-07 12:29:42 -0300

    fix

---
+++ Aula_2/Aula_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aula_2
+//
+//  Created by IOS SENAC on 07/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
