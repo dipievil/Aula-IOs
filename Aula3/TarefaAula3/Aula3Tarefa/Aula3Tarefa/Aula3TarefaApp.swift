3898f71a7ecd0b070abcd8e0a281b64f0deb553c
Diego Ritzel <dipi.evil@gmail.com>
2021-08-14 16:43:36 -0300

    Tarefa

---
+++ Aula3/TarefaAula3/Aula3Tarefa/Aula3Tarefa/Aula3TarefaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aula3TarefaApp.swift
+//  Aula3Tarefa
+//
+//  Created by IOS SENAC on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Aula3TarefaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aula3/TarefaAula3/FormularioDeCadastro.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  App3B
+//
+//  Created by IOS SENAC on 14/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var texto = ""
+    @State private var mostrarAlerta = false
+    @State var idade = 0
+    
+    var body: some View {
+        
+        VStack(alignment: .center, spacing: 10){
+            HStack(alignment: .center, spacing: 10){
+                Text("Valor")
+                
+                TextField("Digite o valor", text: $texto).border(Color.red)
+                
+            }.foregroundColor(.gray)
+            
+            HStack(alignment: .center, spacing: 10){
+                Text("Idade: ")
+                
+                Stepper(value: $idade, in: 0...120){
+                    Text("\(self.idade) anos")
+                }
+            }
+            
+            Quadradinhos()
+            
+            Button(action: {
+                mostrarAlerta = true
+                print("Print mostrou alerta")
+            }){
+                Text("Diga oi")
+                    .padding(.horizontal, 20.0)
+                    .padding(.vertical, 10)
+            }
+            
+            Quadradinhos()
+            
+            Text("VStack").font(.footnote)
+        }
+        .padding([.top, .leading, .bottom], 20.0)
+        .alert(isPresented: $mostrarAlerta){ () -> Alert in
+            return Alert(title: Text("Olá"),
+                         message: Text("Olá \(texto)"),
+                         dismissButton: .default(Text("OK"))
+            )}
+    }
+}
+
+
+struct Quadradinhos : View{
+    var body: some View{
+        ZStack(alignment: .center, content: {
+            Text("-----------")
+        }).foregroundColor(Color.orange)
+        .background(Color.white)
+        .padding(10)
+        .border(Color.gray,width:10)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
