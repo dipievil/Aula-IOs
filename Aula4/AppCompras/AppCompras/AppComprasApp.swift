ee8dc33451fac5e156d58611fed28d7bff242fa3
Diego Ritzel <dipi.evil@gmail.com>
2021-08-21 15:50:13 -0300

    Aula 4 adjust

--- Aula4/AppCompras/AppCompras/AppComprasApp.swift
+++ Aula4/AppCompras/AppCompras/AppComprasApp.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 @main
 struct AppComprasApp: App {
+    
+    let persistenceController = PersistenceController.banco
+    
+    @Environment( \.scenePhase) var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            
+        }.onChange(of: scenePhase){ (newScenePhase) in
+            switch newScenePhase {
+            case.background:
+                persistenceController.save()
+            case.inactive:
+                print ("Está inativo")
+            case.active:
+                print ("Está Inativo")
+            @unknown default:
+                print("Default")
+            }
+            
         }
     }
 }
