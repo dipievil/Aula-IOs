076eaa695e6968802eaf32435ef9a607e4765cc4
Diego Ritzel <dipi.evil@gmail.com>
2021-09-04 15:19:40 -0300

    Alterado escopo para cadastro de livros

--- Aula5/app_loja/app_loja/app_loja/ContentView.swift
+++ Aula5/app_loja/app_loja/app_loja/ContentView.swift
@@ -1,44 +1,48 @@
-//
-//  ContentView.swift
-//  AppLoja
-//
-//  Created by Adalto Selau Sparremberger on 04/09/21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     
     @State var mostrarModal = false
     @State var mostrarTelaCheia = false
-        
     
     var body: some View {
-        NavigationView{
-            List{
-                Section(header: Text("Listar")){
-                    NavigationLink("Clientes", destination: ClientesView())
-                    NavigationLink("Produtos", destination: ProdutosView())
-                }
-                Section(header: Text("Configurações")){
-                    Button("Abrir Modal"){
-                        mostrarModal.toggle()
-                    }.sheet(isPresented: $mostrarModal){
-                        NavigationView{
-                            Color.red.navigationBarTitle("Tela do Modal", displayMode: .inline)
+                
+        TabView{
+            VStack{
+                Text("Biblioteca Online").fontWeight(.bold).font(.system(size: 64.0))
+                Text("The books is on the table").font(.system(size:24))
+            }
+                        
+            NavigationView{
+                List{
+                    Section(header: Text("Listar")){
+                        NavigationLink("Cadastro de Livros", destination: CadastraLivrosView())
+                        NavigationLink("Listagem de livros", destination: ListaLivrosView())
+                    }
+                    Section(header: Text("Configurações")){
+                        
+                        Button("Abrir Modal"){
+                            mostrarModal.toggle()
+                        }.sheet(isPresented: $mostrarModal){
+                            NavigationView{
+                                Color.red.navigationBarTitle("Tela do Modal", displayMode: .inline)
+                            }
+                        }
+                        
+                        
+                        if #available(iOS 14, *){
+                            Button("Sobre"){
+                                mostrarTelaCheia.toggle()
+                            }.fullScreenCover(isPresented: $mostrarTelaCheia, content: {
+                                TelaCheiaView(mostrarTela: $mostrarTelaCheia)
+                            })
                         }
                     }
-                    
-                    if #available(iOS 14, *){
-                        Button("Sobre"){
-                            mostrarTelaCheia.toggle()
-                        }.fullScreenCover(isPresented: $mostrarTelaCheia, content: {
-                            TelaCheiaView(mostrarTela: $mostrarTelaCheia)
-                        })
-                    }
-                }
-            }.navigationBarTitle("AppLoja", displayMode: .inline)
-        }.navigationViewStyle(StackNavigationViewStyle())
+                }.navigationBarTitle("Biblioteca Online", displayMode: .inline)
+            }.navigationViewStyle(StackNavigationViewStyle())
+            
+        }.padding(.all, 10)
+        .tabViewStyle(PageTabViewStyle())
     }
 }
 
@@ -56,10 +60,45 @@ struct TelaCheiaView: View {
     }
 }
 
-
-struct ClientesView: View {
+struct ListaLivrosView: View {
+    @Environment(\.managedObjectContext) var managedObjectContext
     
-    @State var txtNomeCliente = ""
+    // enviroment construído para permitir fechar a View
+    @Environment(\.presentationMode) var presentation
+    
+    @FetchRequest(
+        entity: Livro.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Livro.nomeLivro, ascending: true)])
+    
+    var livros: FetchedResults<Livro>
+    
+    var body: some View{
+        VStack{
+            List{
+                ForEach( livros , id: \.self){ livro in
+                    Text("\(livro.nomeLivro ?? " -- ") ")
+                }
+                .onDelete(perform: removerLivro)
+            }
+            
+        }.padding()
+        .navigationBarTitle("Lista de Livros", displayMode: .inline)
+    }
+    
+    //Função para excluir Livros
+    func removerLivro(at offset: IndexSet){
+        for index in offset{
+            let cli = livros[index]
+            PersistenceController.banco.delete( cli )
+        }
+    }
+}
+
+struct CadastraLivrosView: View {
+    
+    @State var txtNomeLivro = ""
+    @State var txtAutorLivro = ""
+    @State var txtAnoLivro = ""
     
     @Environment(\.managedObjectContext) var managedObjectContext
     
@@ -67,38 +106,30 @@ struct ClientesView: View {
     @Environment(\.presentationMode) var presentation
     
     @FetchRequest(
-        entity: Cliente.entity(),
-        sortDescriptors: [NSSortDescriptor(keyPath: \Cliente.nomeCli, ascending: true)])
-    var clientes: FetchedResults<Cliente>
+        entity: Livro.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Livro.nomeLivro, ascending: true)])
+    var livros: FetchedResults<Livro>
     
     var body: some View{
         VStack{
-            List{
-                ForEach( clientes , id: \.self){ cli in
-                    Text("\(cli.nomeCli ?? " -- ") ")
-                }
-                .onDelete(perform: removerCliente)
-            }
-            HStack{
-                TextField("Digite o nome do cliente", text: $txtNomeCliente )
+            VStack{
+                TextField("Nome do livro", text: $txtNomeLivro )
+                TextField("Nome do autor", text: $txtAutorLivro )
+                TextField("Ano do livro", text: $txtAnoLivro )
+                    
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-                Button("Salvar Cliente"){
-                    let cliente = Cliente(context: managedObjectContext)
-                    cliente.nomeCli = self.txtNomeCliente
+                Button("Adicionar livro"){
+                    let livro = Livro(context: managedObjectContext)
+                    livro.nomeLivro = self.txtNomeLivro
+                    livro.autorLivro = self.txtAutorLivro
+                    livro.anoLivro = self.txtAnoLivro
                     PersistenceController.banco.save()
-                    self.txtNomeCliente = ""
+                    self.txtNomeLivro = ""
                     self.presentation.wrappedValue.dismiss()
                 }
             }
         }.padding()
-        .navigationBarTitle("Lista de Clientes", displayMode: .inline)
-    }
-    //Função para excluir Clientes
-    func removerCliente(at offset: IndexSet){
-        for index in offset{
-            let cli = clientes[index]
-            PersistenceController.banco.delete( cli )
-        }
+        .navigationBarTitle("Cadastro de Livros", displayMode: .inline)
     }
 }
 
@@ -108,14 +139,15 @@ struct ProdutosView: View {
     @State var txtNomeProduto = ""
     
     @Environment(\.managedObjectContext) var managedObjectContext
-        
+        /*
     @FetchRequest(
         entity: Produto.entity(),
         sortDescriptors: [NSSortDescriptor(keyPath: \Produto.nomeProd, ascending: true)])
     var produtos: FetchedResults<Produto>
-    
+    */
     var body: some View{
         VStack{
+            /*
             List{
                 ForEach( produtos , id: \.self){ prod in
                     Text("\(prod.nomeProd ?? " -- ") ")
@@ -132,17 +164,9 @@ struct ProdutosView: View {
                     self.txtNomeProduto = ""
                 }
             }
+             */
         }.padding()
         .navigationBarTitle("Lista de Produtos", displayMode: .large)
-
-    }
-    
-    //Função para excluir Produtos
-    func removerProduto(at offset: IndexSet){
-        for index in offset{
-            let prod = produtos[index]
-            PersistenceController.banco.delete( prod )
-        }
     }
 }
 
