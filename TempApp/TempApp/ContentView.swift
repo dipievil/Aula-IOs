866783824b45a7db400350fc24baf19eaaa31be8
Diego Ritzel <dipi.evil@gmail.com>
2021-09-25 12:13:42 -0300

    Missing files

---
+++ TempApp/TempApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TempApp
+//
+//  Created by IOS SENAC on 11/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
